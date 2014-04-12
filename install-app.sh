96d90abf207b458d483058b5464aac9bbf85afc5
Andrew Pearce <andy@wiretaplabs.com>
2014-04-12 13:02:57 -0500

    Added supporting files and directories
    Added pom.xml, install-app.sh and other supporting files

---
+++ install-app.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+cd $HOME/OpenDaylight/controller/opendaylight
+cp ./samples/ONFTappingApp/target/samples.onftappingapp-0.4.2-SNAPSHOT.jar \
+   ./distribution/opendaylight/target/distribution.opendaylight-osgipackage/opendaylight/plugins
+
